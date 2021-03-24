f7d61f09a2038e7883f43809649f7c440ffc0579
mabroor007 <amabroor007@gmail.com>
2021-03-24 22:12:25 +0500

    array reversal algoritum

--- arrays/index.ts
+++ arrays/index.ts
@@ -18,10 +18,40 @@ export function leftRotateArrayByOne(arr: any[]): any[] {
 
 // Shift the elements of array by n
 export function leftRotateArrayBy(arr: any[], times: number): any[] {
-
   for (let i = 0; i < times; i++) {
     arr = leftRotateArrayByOne(arr);
   }
 
   return arr;
+}
+
+// Reverses the elements of array
+export function reverseArray(arr: any[]): any[] {
+  let reversedArray = [];
+  for (let i in arr) {
+    reversedArray.push(arr[arr.length - Number(i) - 1]);
+  }
+  return reversedArray;
+}
+
+export function rightRotateByReversalAlgorithum(
+  arr: any[],
+  times: number
+): any[] {
+  if (arr.length === 0) return [];
+  let combinedReversedArray = [];
+  // split the array n - times
+  let splitIndex = arr.length - times;
+
+  // Loop left to right until splitindex and insert element at begining
+  for (let i = 0; i < splitIndex; i++) {
+    combinedReversedArray.unshift(arr[i]);
+  }
+
+  // Loop right to left til splitindex and insert element at end
+  for (let i = arr.length - 1; i >= splitIndex; i--) {
+    combinedReversedArray.push(arr[i]);
+  }
+
+  return reverseArray(combinedReversedArray);
 }
--- main.ts
+++ main.ts
@@ -2,6 +2,21 @@ import * as algo from "./arrays";
 
 const myarr: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-const result = algo.leftRotateArrayBy(myarr, 1);
+function blockSwapArray(arr: any[], times: number): any[] {
+  if (arr.length === 0) return [];
 
-console.log(result);
+  let A = [];
+  let B = [];
+
+  for (let i = 0; i <= times - 1; i++) {
+    A.push(arr[i]);
+  }
+
+  for (let i = times; i <= arr.length - 1; i++) {
+    B.push(arr[i]);
+  }
+
+  return [A,B];
+}
+
+console.log(blockSwapArray(myarr, 2));
